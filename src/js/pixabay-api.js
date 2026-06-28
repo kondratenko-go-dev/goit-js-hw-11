e2206731192da7346c6b0bf1d3f6b0f662ae6ab2
kondratenko-go-dev <kondratenko.aleksandr.go.dev@gmail.com>
2026-06-28 13:22:34 +0300

    task-11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,100 +1,15 @@
 import axios from 'axios';
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const form = document.querySelector('#search-form');
-const input = document.querySelector('input[name="searchQuery"]');
-const gallery = document.querySelector('.gallery');
-const loader = document.querySelector('.loader');
-
-const API_KEY = '51262986-c338856f457aa770ab0affaea';
-const BASE_URL = 'https://pixabay.com/api/';
-
-const lightbox = new SimpleLightbox('.gallery a');
-
-function showLoader() {
-  loader.style.display = 'block';
-}
-function hideLoader() {
-  loader.style.display = 'none';
-}
-
-function clearGallery() {
-  gallery.innerHTML = '';
-}
-
-function createMarkup(images) {
-  return images
-    .map(
-      ({
-         webformatURL,
-         largeImageURL,
-         tags,
-         likes,
-         views,
-         comments,
-         downloads,
-       }) => `
-    <a href="${largeImageURL}" class="photo-card">
-      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-      <div class="info">
-        <p><b>Likes:</b> ${likes}</p>
-        <p><b>Views:</b> ${views}</p>
-        <p><b>Comments:</b> ${comments}</p>
-        <p><b>Downloads:</b> ${downloads}</p>
-      </div>
-    </a>
-  `
-    )
-    .join('');
-}
-
-async function fetchImages(query) {
-  const params = {
-    key: API_KEY,
+export default function getImagesByQuery(query) {
+  const searchParam = new URLSearchParams({
+    key: '56012300-f086c257a570084238c9733eb',
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
-  };
+  });
+  const URL = `https://pixabay.com/api/`;
 
-  try {
-    const { data } = await axios.get(BASE_URL, { params });
-    return data.hits;
-  } catch (error) {
-    console.error('API Hatası:', error);
-    throw error;
-  }
+  return axios.get(URL, { params: searchParam })
+    .then(response => response.data);
 }
-
-form.addEventListener('submit', async e => {
-  e.preventDefault();
-  const query = input.value.trim();
-  if (!query) return;
-
-  clearGallery();
-  showLoader();
-
-  try {
-    const images = await fetchImages(query);
-    if (images.length === 0) {
-      iziToast.error({
-        title: 'Oops',
-        message: 'No images found. Try a different keyword.',
-      });
-      return;
-    }
-
-    gallery.innerHTML = createMarkup(images);
-    lightbox.refresh();
-  } catch (err) {
-    iziToast.error({
-      title: 'Error',
-      message: 'Something went wrong while fetching images.',
-    });
-  } finally {
-    hideLoader();
-  }
-});
